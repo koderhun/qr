6518b034f6866b56f99c5f5b37b24967db3dafc4
Ramil <example@gmail.com>
2023-08-05 06:01:35 +0300

    предварительная версия

--- src/components/WifiQr/WifiQr.tsx
+++ src/components/WifiQr/WifiQr.tsx
@@ -1,205 +1,124 @@
-import {useEffect, useState} from 'react'
-import QrCodeLayout from '@/components/QrCodeLayout/QrCodeLayout'
-import {Controller, useForm} from 'react-hook-form'
-import {
-  Box,
-  FormControl,
-  Button,
-  TextField,
-  Autocomplete,
-  FormHelperText,
-} from '@mui/material'
+import {Box, ToggleButtonGroup, ToggleButton} from '@mui/material'
+import {useForm, SubmitHandler, FormProvider} from 'react-hook-form'
+import {literal, object, string, TypeOf} from 'zod'
 import {zodResolver} from '@hookform/resolvers/zod'
-import {z} from 'zod'
-import s from './styles.module.scss'
+import {useEffect, useState} from 'react'
+import {LoadingButton} from '@mui/lab'
+import FormInput from '@/components/FormInput/FormInput'
+import QrCodeLayout from '@/components/QrCodeLayout/QrCodeLayout'
+import {wifiTemplateString} from '@/utils/getWifiTemplateString'
 
-const wifiNameSchema = z
-  .string()
-  .transform((value) => value.replace(/\D/gu, ''))
-  .refine((val) => val.length > 10, 'Название не может быть больше 10 символов')
-  .refine((val) => val.startsWith('04'), 'Название не может содержать "04"')
+const wifiSchema = object({
+  ssid: string()
+    .nonempty('SSID is required')
+    .max(32, 'Name must be less than 100 characters'),
+  password: string().max(32, 'Password must be less than 32 characters'),
+  // typePassword
+})
 
-const cryptMethod = ['None', 'WPA', 'WEP'] as const
+type WifiInput = TypeOf<typeof wifiSchema>
 
-const looseOptional = <T extends z.ZodTypeAny>(schema: T) =>
-  z.preprocess(
-    (value: unknown) =>
-      value === null || (typeof value === 'string' && value === '')
-        ? undefined
-        : value,
-    schema.optional(),
-  )
+type wifiCryptMethodItem = {
+  name: string
+  value: string
+}
 
-const formSchema = z
-  .object({
-    ssid: looseOptional(
-      z.string().nonempty('Название точки доступа не может быть пустым'),
-    ),
-    pass: looseOptional(
-      z.string().nonempty('Название точки доступа не может быть пустым'),
-    ),
-    preferredCryptMethod: z.enum(cryptMethod, {
-      required_error: 'Required Error!',
-      invalid_type_error: 'Invalid Error!',
-    }),
-  })
-  .superRefine((values, context) => {
-    if (
-      values.preferredCryptMethod === 'WPA' ||
-      values.preferredCryptMethod === 'WEP'
-    ) {
-      context.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Пароль обязателен!',
-        path: ['pass'],
-      })
-    }
-  })
-
-type FormFields = z.infer<typeof formSchema>
+const wifiCryptMethodOptions: wifiCryptMethodItem[] = [
+  {name: 'None', value: 'none'},
+  {name: 'WEP', value: 'WEP'},
+  {
+    name: 'WPA',
+    value: 'WPA',
+  },
+]
 
 const WifiQr = () => {
+  const [loading, setLoading] = useState(false)
+  const [wifiCryptMethodSelected, setWifiCryptMethodSelected] = useState('none')
   const [qrCodeText, setQrCodeText] = useState('')
-  const {control, handleSubmit, watch, trigger, getValues} =
-    useForm<FormFields>({
-      mode: 'onBlur',
-      reValidateMode: 'onBlur',
-      resolver: zodResolver(formSchema),
-    })
 
-  useEffect(() => {
-    const subscription = watch((_value, {name}) => {
-      if (name === 'preferredCryptMethod') {
-        void trigger()
-      }
-    })
+  const wifiCryptMethodChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newWifiCryptMethod: string,
+  ) => {
+    setWifiCryptMethodSelected(newWifiCryptMethod)
+  }
 
-    return () => subscription.unsubscribe()
-  }, [watch, trigger])
+  const methods = useForm<WifiInput>({
+    resolver: zodResolver(wifiSchema),
+  })
+
+  const {
+    reset,
+    handleSubmit,
+    register,
+    formState: {isSubmitSuccessful, errors},
+  } = methods
+
+  const onSubmitHandler: SubmitHandler<WifiInput> = (values) => {
+    const {ssid, password} = values
+    console.log('values', values)
+    setQrCodeText(
+      wifiTemplateString({
+        ssid,
+        pass: password,
+        crypt: wifiCryptMethodSelected,
+      }),
+    )
+  }
+  console.log('Errors:', errors)
 
   return (
-    <>
-      <QrCodeLayout qrCodeText={qrCodeText}>
-        <Box
-          component='form'
-          autoComplete='off'
-          onSubmit={handleSubmit((data) => console.log('data: ', data))}
-          noValidate
-          sx={{mt: 0}}
-        >
-          <Controller
-            name='ssid'
-            control={control}
-            render={({
-              field: {value, onChange, onBlur, ref},
-              fieldState: {error},
-            }) => (
-              <FormControl>
-                <TextField
-                  name='ssid'
-                  label='SSID'
-                  placeholder='wifi name'
-                  inputRef={ref}
-                  value={value}
-                  onChange={onChange}
-                  onBlur={onBlur}
-                  error={Boolean(error)}
-                  required={true}
-                />
-                <FormHelperText
-                  sx={{
-                    color: 'error.main',
-                  }}
-                >
-                  {error?.message ?? ''}
-                </FormHelperText>
-              </FormControl>
-            )}
-          />
-          <Controller
-            name='pass'
-            control={control}
-            render={({
-              field: {value, onChange, onBlur, ref},
-              fieldState: {error},
-            }) => (
-              <FormControl>
-                <TextField
-                  name='pass'
-                  label='Password'
-                  placeholder='password'
-                  inputRef={ref}
-                  value={value}
-                  onChange={onChange}
-                  onBlur={onBlur}
-                  error={Boolean(error)}
-                  required={
-                    getValues('preferredCryptMethod') === 'WEP' ||
-                    getValues('preferredCryptMethod') === 'WPA'
-                  }
-                />
-                <FormHelperText
-                  sx={{
-                    color: 'error.main',
-                  }}
-                >
-                  {error?.message ?? ''}
-                </FormHelperText>
-              </FormControl>
-            )}
-          />
-
-          <Controller
-            name='preferredCryptMethod'
-            control={control}
-            render={({
-              field: {value, onChange, onBlur, ref},
-              fieldState: {error},
-            }) => (
-              <FormControl>
-                <Autocomplete
-                  onChange={(
-                    _event: unknown,
-                    item: (typeof cryptMethod)[number] | null,
-                  ) => {
-                    onChange(item)
-                  }}
-                  value={value}
-                  options={cryptMethod}
-                  sx={{width: 245}}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      error={Boolean(error)}
-                      onBlur={onBlur}
-                      inputRef={ref}
-                      label={'Crypto Type'}
-                      defaultValue={cryptMethod[0]}
-                    />
-                  )}
-                />
-                <FormHelperText
-                  sx={{
-                    color: 'error.main',
-                  }}
-                >
-                  {error?.message ?? ''}
-                </FormHelperText>
-              </FormControl>
-            )}
-          />
-
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{mt: 3, mb: 2}}
+    <QrCodeLayout qrCodeText={qrCodeText}>
+      <Box>
+        <FormProvider {...methods}>
+          <Box
+            component='form'
+            noValidate
+            autoComplete='off'
+            onSubmit={handleSubmit(onSubmitHandler)}
           >
-            Generate
-          </Button>
-        </Box>
-      </QrCodeLayout>
-    </>
+            <FormInput
+              type='text'
+              name='ssid'
+              required
+              fullWidth
+              label='SSID'
+              sx={{mb: 2}}
+            />
+            <FormInput
+              type='text'
+              name='password'
+              fullWidth
+              label='Password'
+              sx={{mb: 2}}
+            />
+
+            <ToggleButtonGroup
+              color='primary'
+              value={wifiCryptMethodSelected}
+              exclusive
+              onChange={wifiCryptMethodChange}
+              aria-label='Platform'
+            >
+              {wifiCryptMethodOptions.map((v) => (
+                <ToggleButton value={v.value}>{v.name}</ToggleButton>
+              ))}
+            </ToggleButtonGroup>
+
+            <LoadingButton
+              variant='contained'
+              fullWidth
+              type='submit'
+              loading={loading}
+              sx={{py: '0.8rem', mt: '1rem'}}
+            >
+              Generate
+            </LoadingButton>
+          </Box>
+        </FormProvider>
+      </Box>
+    </QrCodeLayout>
   )
 }
 
